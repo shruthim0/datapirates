5ed059f4af5cf14054eb7d5b9db3899d1fef784c
Ananyag2617 <Ananyag2617@users.noreply.github.com>
2023-02-16 22:35:17 +0000

    deploy: 7d04eb811fd0467f29f7e198638228538f878969

--- _pages/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request
-from nutrition import nutrition_driver 
-app = Flask(__name__)
-
-@app.route('/nutriition/', methods=['GET','POST'])
-def Nutrition():
-    if request.form:
-        nutrition_driver()
-
